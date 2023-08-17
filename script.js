79065434f2cf6387bc614867737a41682a9aa8e9
Antônio Pinheiro <matoscezar17@gmail.com>
2023-08-17 10:11:37 -0300

    Organizando o codigo

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("menu-hamburguer").addEventListener("click", function() {
+    document.getElementById("sidebar").style.right = "0";
+    });
+
+    document.getElementById("close-menu").addEventListener("click", function() {
+    document.getElementById("sidebar").style.right = "-300px";
+    });
+    $(document).ready(function(){
+$('#carrosel-imagem').slick({
+    autoplay: true,
+});
+
+})
